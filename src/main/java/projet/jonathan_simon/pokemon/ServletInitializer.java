213c0e20efbce7b467fc1fec6a712228ba6cb090
Jonathan <jonathan.martin@livecampus.tech>
2023-07-31 11:54:37 +0200

    Yo le rap

---
+++ src/main/java/projet/jonathan_simon/pokemon/ServletInitializer.java
@@ -0,0 +1,13 @@
+package projet.jonathan_simon.pokemon;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PokemonApplication.class);
+    }
+
+}
---
+++ src/test/java/projet/jonathan_simon/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package projet.jonathan_simon.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
